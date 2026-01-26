90ded62cfd5ceb5be1ff521edd1863a9a2e5ed14
NISHANTH THAKKALLAPALLY <88816230+Nishanth2468@users.noreply.github.com>
2026-01-26 23:38:46 +0530

    Refactor duplicate finding logic in array-duplicates.py
    Multiple approaches for finding the duplicates in an array.

--- Difficulty: Easy/Array Duplicates/array-duplicates.py
+++ Difficulty: Easy/Array Duplicates/array-duplicates.py
@@ -2,27 +2,53 @@ class Solution:
     def findDuplicates(self, arr):
         # code here
         # n = len(arr)
-        # duplecate = []
+        # duplicates = []
         # # count = 0
         
         # for i in range(n) :
         #     for j in range(i + 1, n) :
         #         if arr[i] == arr[j] :
-        #             duplecate.append(arr[i])
-                    
-                    
+        #             duplicates.append(arr[i])
+                               
         # return duplecate
+
         
+        # Approach 2
+        # duplicates = []
+        # freq = {}
+        
+        # for num in arr :
+        #     freq[num] = freq.get(num, 0) + 1
+            
+        # for key in freq :
+        #     if freq[key] > 1 :
+        #         duplicates.append(key)
+                
+        # return duplicates
+
+        # This approach 3 is really good
+        # res = []
+        # for i in range(len(arr)):
+        #     idx = abs(arr[i]) - 1
+                    
+        #     if arr[idx] < 0:
+        #         res.append(abs(arr[i]))
+        #     else:
+        #         arr[idx] = -arr[idx]
+        
+        # return res
+
+        # Approach 4:
         seen = set()
-        duplecates = []
+        duplicates = []
         
         for i in arr :
             if i in seen :
-                # if i not in duplecates : to avoid repeating duplicates
-                duplecates.append(i)
+                # if i not in duplicates: to avoid repeating duplicates
+                duplicates.append(i)
             else :
                 seen.add(i)
                     
-        return duplecates
+        return duplicates
         
         
