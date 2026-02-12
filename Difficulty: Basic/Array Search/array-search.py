8de2253e90be64ecbbbf8c2d7a9ad6e04c686086
NISHANTH THAKKALLAPALLY <88816230+Nishanth2468@users.noreply.github.com>
2026-02-12 19:46:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array Search/array-search.py
+++ Difficulty: Basic/Array Search/array-search.py
@@ -1,11 +1,16 @@
+
+from typing import List
+
 class Solution:
-    def search(self, arr, x):
+    def search(self, k: int, arr: List[int]) -> int:
         # code here
         n = len(arr)
         
-        for num in range(n) :
-            if arr[num] == x :
-                return num
-                
+        for i in range(n):
+            size = i+1
+            if arr[i] == k:
+                return size
+                break
+        
         return -1
-            
+        
